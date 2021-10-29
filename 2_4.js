49a938783d7adee4ddd2356f36cc6ee8f8a94916
Annet <hlanastasiya@mail.ru>
2021-10-29 21:39:10 +0300

    lesson 2

---
+++ 2_4.js
@@ -0,0 +1,16 @@
+function sum(k, m) {
+    return k + m;
+    }
+    
+    function min(k, m) {
+        return k - m;
+    }
+    
+    function mult(k, m) {
+        return k * m;
+    }
+    
+    function div(k, m) {
+        return k / m;
+    }
+    
